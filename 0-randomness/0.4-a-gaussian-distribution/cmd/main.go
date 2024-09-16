23b1cbbd87b133d2dbfd95ee2037cbdeb5630223
vinibispo <vini.bispo015@gmail.com>
2024-09-16 08:02:14 -0300

    Add Basic draw of a Gaussian Distribution

---
+++ 0-randomness/0.4-a-gaussian-distribution/cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func randomGaussian() float64 {
+	u1 := rand.Float64()
+	u2 := rand.Float64()
+	return math.Sqrt(-2*math.Log(u1)) * math.Cos(2*math.Pi*u2)
+}
+
+func main() {
+	rl.InitWindow(640, 240, "A Gaussian Distribution")
+	rl.SetTargetFPS(60)
+	defer rl.CloseWindow()
+	target := rl.LoadRenderTexture(640, 240)
+	defer rl.UnloadRenderTexture(target)
+	rl.BeginTextureMode(target)
+	rl.ClearBackground(rl.RayWhite)
+	rl.EndTextureMode()
+	for !rl.WindowShouldClose() {
+		x := float32(randomGaussian()*60 + 320)
+		rl.BeginTextureMode(target)
+		rl.DrawCircleV(rl.NewVector2(x, 120), 8, rl.NewColor(0, 0, 0, 10))
+		rl.EndTextureMode()
+
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawTextureRec(target.Texture, rl.NewRectangle(0, 0, float32(target.Texture.Width), -float32(target.Texture.Height)), rl.Vector2Zero(), rl.White)
+		rl.EndDrawing()
+	}
+}
